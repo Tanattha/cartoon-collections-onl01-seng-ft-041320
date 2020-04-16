1b21810866f9c4584d2a93c7a6e520859f2d4d12
Tanattha Thuenim <tanattha.thuenim@gmail.com>
2020-04-16 01:30:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,7 @@ def long_planeteer_calls(array)
     false
  end
  end
+ planeteer_calls.any? { |call| call.length > 4 }
 
 def find_the_cheese# code an argument here
   # the array below is here to help

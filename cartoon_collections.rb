b9b7033d377f10749fb96c33440ca0c7769be048
Tanattha Thuenim <tanattha.thuenim@gmail.com>
2020-04-16 01:23:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,10 +5,12 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array do |name|
+  array.collect do |name|
     puts "#{name}!"
   end
 end
+
+
 
 def long_planeteer_calls# code an argument here
   # Your code here
